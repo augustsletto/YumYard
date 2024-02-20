1796f5ebcb617cdd0354d3a1476b83e8c02d0fe3
august sletto <augustsletto@gmail.com>
2024-02-20 18:28:55 +0000

    Superuser added, env.py added, connected to sql

--- yumyard/urls.py
+++ yumyard/urls.py
@@ -20,7 +20,7 @@ from home import views as index_views
 from about import views as about_views
 
 urlpatterns = [
-    path('hello/', index_views.index, name='index'),
+    path('', index_views.index, name='index'),
     path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
 ]
