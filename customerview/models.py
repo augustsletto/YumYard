df182f82468b498afbd309cd9352c8af10a0e2d2
august sletto <augustsletto@gmail.com>
2024-02-21 12:45:00 +0000

    Customerviews model corrected

--- customerview/models.py
+++ customerview/models.py
@@ -22,7 +22,7 @@ class Category(models.Model):
 class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    items = models.ManyToManyField('MenuItems', related_name='order', blank=True)
+    items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
 
     def __str__(self):
         return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
