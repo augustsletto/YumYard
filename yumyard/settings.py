71c5329f905d6ab382c4c82be23c1211c3d8aeb3
august sletto <augustsletto@gmail.com>
2024-02-22 23:43:47 +0000

    search bar in menu

--- yumyard/settings.py
+++ yumyard/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-augustslett-hotelbookin-rjz4293wgjg.ws-eu108.gitpod.io','.herokuapp.com']
 
