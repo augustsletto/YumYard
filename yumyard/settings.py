b2d517d6c2283a9e14da5da0f52b8a5acc6e477b
august sletto <augustsletto@gmail.com>
2024-02-25 22:28:20 +0000

    change image sources

--- yumyard/settings.py
+++ yumyard/settings.py
@@ -161,8 +161,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
